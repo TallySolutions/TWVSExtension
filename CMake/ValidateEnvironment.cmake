930a371360770f0261ca6775c1d82c6a1fe29ec3
Mohsin Siddiqui <82157778+mohsin-52715@users.noreply.github.com>
2025-07-18 13:41:59 +0530

    Update ValidateEnvironment.cmake

--- CMake/ValidateEnvironment.cmake
+++ CMake/ValidateEnvironment.cmake
@@ -160,6 +160,8 @@ macro (setup_usr_share_folder)
             RESULT_VARIABLE retCode
             OUTPUT_VARIABLE out
             ERROR_VARIABLE error
+	    COMMAND_ECHO STDOUT
+            COMMAND_ERROR_IS_FATAL ANY
         )
 endmacro()
 
